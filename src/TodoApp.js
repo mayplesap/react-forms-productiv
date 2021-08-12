57b90b4d40658c11edf23adf53070785decb5f16
ZeeTom <zthom13@hotmail.com>
2021-08-12 15:58:44 -0600

    worked on TodoForm

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
+import TodoForm from "./TodoForm";
 
 import TopTodo from "./TopTodo";
 import EditableTodoList from "./EditableTodoList";
@@ -16,45 +17,38 @@ import EditableTodoList from "./EditableTodoList";
  */
 
 function TodoApp() {
-
   /** add a new todo to list */
-  function create(newTodo) {
-  }
+  function create(newTodo) {}
 
   /** update a todo with updatedTodo */
-  function update(updatedTodo) {
-  }
+  function update(updatedTodo) {}
 
   /** delete a todo by id */
-  function remove(id) {
-  }
+  function remove(id) {}
 
   /**TODO: handleSave: for the form */
 
   return (
-      <main className="TodoApp">
-        <div className="row">
-
-          <div className="col-md-6">
-            <EditableTodoList /> OR
-            <span className="text-muted">You have no todos.</span>
-          </div>
-
-          <div className="col-md-6">
-            (if no top todo, omit this whole section)
-            <section className="mb-4">
-              <h3>Top Todo</h3>
-              <TopTodo />
-            </section>
-
-            <section>
-              <h3 className="mb-3">Add Nü</h3>
-              FIXME
-            </section>
-          </div>
-
+    <main className="TodoApp">
+      <div className="row">
+        <div className="col-md-6">
+          <EditableTodoList /> OR
+          <span className="text-muted">You have no todos.</span>
         </div>
-      </main>
+
+        <div className="col-md-6">
+          (if no top todo, omit this whole section)
+          <section className="mb-4">
+            <h3>Top Todo</h3>
+            <TopTodo />
+          </section>
+          <section>
+            <h3 className="mb-3">Add Nü</h3>
+            <TodoForm />
+          </section>
+        </div>
+      </div>
+    </main>
   );
 }
 
